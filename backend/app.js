6d8cbbeb3eade01ff2724c75a09c7850bd879790
aayushgoyal9507 <aayushgoyal9507@gmail.com>
2020-01-22 15:49:24 -0700

    Add Post edit functionality and Spinner while loading

--- backend/app.js
+++ backend/app.js
@@ -1,62 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://aayush9507:aayush9507@cluster0-kvia0.mongodb.net/node-angular?retryWrites=true&w=majority")
-.then( ()=> {
-  console.log('Connected to MongoDB database!!;)');
-})
-.catch( ()=> {
-  console.log('Connection failed!!');
-})
 
-
-const Post = require('./models/post');
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
+mongoose
+  .connect(
+    "mongodb+srv://aayush9507:aayush9507@cluster0-kvia0.mongodb.net/angular-node?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS,PUT");
-
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  // Saving our Post in MongoDB
-  post.save().then(result => {
-    res.status(201).json({
-      message:'post added bro',
-      postId: result._id
-    });
-  });
-});
-
-app.get('/api/posts' , (req, res, next) => {
-
-  Post.find().then(documents => {
-    res.status(200).json({
-      message: 'Posts fetched success',
-      posts: documents
-    });
-  });
-})
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then( result => {
-    console.log(result);
-    res.status(200).json({message: "Post deleted"});
-});
-});
-
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
